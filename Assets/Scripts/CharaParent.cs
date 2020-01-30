910da3ffd72503a51016180e910f5f0b320c8164
toya_akiyama <akym.tohoku@gmail.com>
2020-01-30 13:01:26 +0900

    駒のスーパークラス

---
+++ Assets/Scripts/CharaParent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static DataBase;
+
+public class CharaParent : MonoBehaviour
+{
+    private int HitPoint;
+    private int AttackPower;
+    NowPoint now = new NowPoint();
+    private List<(int,int)> moveRange = new List<(int,int)>();
+    private List<(int,int)> attackRange = new List<(int, int)>();
+    public void Init(int x, int y)
+    {
+        now.xAxis = x;
+        now.yAxis = y;
+    }
+    public void Erase()
+    {
+        Destroy(this.gameObject);
+    }
+    public void AddDamage(int damage)
+    {
+        HitPoint -= damage;
+        if (HitPoint < 0) Erase();
+    }
+    public List<(int,int)> Movable()
+    {
+        List<(int,int)> res = new List<(int,int)>();
+        foreach((int,int) T in moveRange)
+        {
+            var (i, j) = T;
+            if (DataBase.CanSet(i, j))
+            {
+                res.Add((i, j));
+            }
+        }
+        return res;
+    }
+    public List<(int, int)> Attackable()
+    {
+        List<(int, int)> res = new List<(int, int)>();
+        foreach ((int, int) T in attackRange)
+        {
+            var (i, j) = T;
+            if (DataBase.CanSet(now.xAxis+i, now.yAxis+j))
+            {
+                res.Add((i, j));
+            }
+        }
+        return res;
+    }
+
+    private void Start()
+    {
+
+    }
+}
+public class NowPoint
+{
+    public int xAxis;
+    public int yAxis;
+}
---
+++ Assets/Scripts/DataBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataBase : MonoBehaviour
+{
+    private static int[,] board;
+    public int vertical;
+    public int horizontal;
+    private void Start()
+    {
+        board = new int[vertical, horizontal];
+    }
+    public void Set(int x,int y,int mobColor)
+    {
+        board[x, y] = mobColor;
+    }
+    public static bool CanSet(int x,int y)
+    {
+        return board[x, y] == 0;
+    }
+
+}
