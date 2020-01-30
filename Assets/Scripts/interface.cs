203f338a13dd4d6ee912fe56eb2a22d992ee48b4
toya_akiyama <akym.tohoku@gmail.com>
2020-01-30 15:40:10 +0900

    初期化

--- Assets/Scripts/interface.cs
+++ Assets/Scripts/interface.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface interFace
+public interface interFace /* CharaParentにかく関数は全部ここに書く。cの ~.hみたいなもん*/
 {
     void AddDamage(int damage);
     string GetName();
