58b3cfe98b5495044b9fb00657306e574bc49d85
kota0126 <watako0126w@gmail.com>
2020-01-30 14:57:37 +0900

    あ

---
+++ Assets/SceneMoving.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMoving : MonoBehaviour
+{
+    public string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
