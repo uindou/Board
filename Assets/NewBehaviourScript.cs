0beb63435197f5420b1de44228c6e04570002197
kota0126 <watako0126w@gmail.com>
2020-01-30 15:39:32 +0900

    画像貼り付け

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    GameObject ChildObject;
+    public Sprite image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChildObject = transform.GetChild(0).gameObject;
+        ChildObject.GetComponent<Image>().sprite = image;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
