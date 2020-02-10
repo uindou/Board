9e08bd5568efc66c393588af08a8c1e9ef5fb20d
kota0126 <watako0126w@gmail.com>
2020-02-10 19:17:58 +0900

    ①地面のテクスチャを変更（仮） ②シーン「AnimationTest」で攻撃・ダメージのアニメを作成

---
+++ Assets/Scripts/AnimeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
+public class AnimeController : MonoBehaviour
+{
+    public Sprite Attack1;
+    public Sprite Attack2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AttackMotion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public async void AttackMotion()
+    {
+
+        while(true)
+        {
+            this.GetComponent<Image>().sprite = Attack1;
+            Debug.Log("Attak1");
+            await Task.Delay(250);
+            this.GetComponent<Image>().sprite = Attack2;
+            Debug.Log("Attak2");
+            await Task.Delay(250);
+        }
+    }
+}
---
+++ Assets/Scripts/DamageAnime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
+public class DamageAnime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DamageMotion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public async void DamageMotion()
+    {
+
+        while(true)
+        {
+            GetComponent<Image>().color = Color.red;
+            Debug.Log("Damage1");
+            await Task.Delay(250);
+            GetComponent<Image>().color = Color.white;
+            Debug.Log("Damage2");
+            await Task.Delay(250);
+        }
+    }
+}
