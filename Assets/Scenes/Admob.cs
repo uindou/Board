e4698c826cdb7145373f91263193f9223df1f4e9
kota0126 <watako0126w@gmail.com>
2020-02-11 13:51:39 +0900

    バナー付けた

---
+++ Assets/Scenes/Admob.cs
@@ -0,0 +1,127 @@
+﻿gleAds.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleAds : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // アプリID、 これはテスト用
+        string appId = "ca-app-pub-3940256099942544~3347511713";
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        RequestBanner();
+    }
+    private void RequestBanner()
+    {
+
+        // 広告ユニットID これはテスト用
+        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+
+        // Create a 320x50 banner at the top of the screen.
+        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+ 
+public class GoogleAds : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // アプリID、 これはテスト用
+        string appId = "ca-app-pub-3940256099942544~3347511713";
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        RequestBanner();
+    }
+    private void RequestBanner()
+    {
+
+        // 広告ユニットID これはテスト用
+        string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+
+        // Create a 320x50 banner at the top of the screen.
+        BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
