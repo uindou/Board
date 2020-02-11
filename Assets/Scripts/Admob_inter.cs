b565401e11e1a562d4673d5e4fa4b86f717d8fca
kota0126 <watako0126w@gmail.com>
2020-02-11 17:20:21 +0900

    なんかした

--- Assets/Scripts/Admob_inter.cs
+++ Assets/Scripts/Admob_inter.cs
@@ -1,39 +1,44 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using GoogleMobileAds.Api;
+
 public class Admob_inter : MonoBehaviour
 {
-    public string Android_Interstitial;
-    private InterstitialAd _interstitial;
-
-    // Use this for initialization
-    void Awake()
-    {
-    }
-
+    private InterstitialAd interStitialView;
     // Use this for initialization
     void Start()
     {
-        // 起動時にインタースティシャル広告をロードしておく
+        // アプリID、 これはテスト用
+        string appId = "ca-app-pub-8801150864537344~8686480864";
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        Debug.Log("Ad loaded");
         RequestInterstitial();
     }
-
-    public void RequestInterstitial()
+    private void RequestInterstitial()
     {
+
+        // 広告ユニットID これはテスト用
         string adUnitId = "ca-app-pub-3940256099942544/1033173712";
 
-        // Initialize an InterstitialAd.
-        _interstitial = new InterstitialAd(adUnitId);
+        // Create an interstitial at the top of the screen.
+        interStitialView = new InterstitialAd(adUnitId);
 
         // Create an empty ad request.
-        AdRequest request = new AdRequest.Builder().AddTestDevice("").Build();
+        AdRequest request = new AdRequest.Builder().Build();
 
-        // Load the interstitial with the request.
-        _interstitial.LoadAd(request);
+        // Load the banner with the request.
+        interStitialView.LoadAd(request);
+
     }
-    void HandleAdClosed(object sender, System.EventArgs e)
+
+
+    // Update is called once per frame
+    void Update()
     {
-        _interstitial.Destroy();
-        RequestInterstitial();
+
     }
 }
