f60e4dd4716f66742c8cec4fbe5129de2766e7f6
kota0126 <watako0126w@gmail.com>
2020-02-11 16:07:14 +0900

    win画面作成

---
+++ Assets/Admob_inter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+public class Admob_inter : MonoBehaviour
+{
+    private InterstitialAd _interstitial;
+
+    // Use this for initialization
+    void Awake()
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // 起動時にインタースティシャル広告をロードしておく
+        RequestInterstitial();
+    }
+
+    public void RequestInterstitial()
+    {
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+
+        // Initialize an InterstitialAd.
+        _interstitial = new InterstitialAd(adUnitId);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().AddTestDevice("").Build();
+
+        // Load the interstitial with the request.
+        _interstitial.LoadAd(request);
+    }
+    void HandleAdClosed(object sender, System.EventArgs e)
+    {
+        _interstitial.Destroy();
+        RequestInterstitial();
+    }
+}
