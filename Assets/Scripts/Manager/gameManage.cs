9a49966b3a60e9d604c25dc87ba3bf43e6e37920
kota0126 <watako0126w@gmail.com>
2020-02-28 16:21:34 +0900

    あ

--- Assets/Scripts/Manager/gameManage.cs
+++ Assets/Scripts/Manager/gameManage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using static DataBase;
 using static gameManage;
 using static myAI;
-using static AdMobManager;
+using static InterstitialManager;
 using UnityEngine.SceneManagement;
 using System.Threading.Tasks;
 
@@ -430,7 +430,7 @@ public class PreEnd : State
             }
             SceneManager.LoadScene("Win");
             //ゲーム終了した後にしたい処理をここに全部書く、他スクリプトの呼び出しとかがいいかも
-            AdMobManager.GameOver();
+            InterstitialManager.GameOver();
             return new End();
         }
     }
