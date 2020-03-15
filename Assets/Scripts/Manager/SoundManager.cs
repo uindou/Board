91cffea93ec70946131e287cb2bff6c47ae14af6
kota0126 <watako0126w@gmail.com>
2020-03-15 17:31:06 +0900

    MainMenuのBGMの大きさを変えられるようにした。PlayerPrefを使えば他シーンでも行けると思う。

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    
-   
-    public void OnClick()
+    public Slider bgmSlider;
+    public Slider seSlider;
+    public AudioSource bgm;
+    public float bgmVolume = 1;
+    public float seVolume = 1;
+
+    void Start()
     {
-        DataBase.bgmflug = !DataBase.bgmflug;
+        bgmSlider.value = bgmVolume;
+        seSlider.value = seVolume;
+    }
+
+    public void ChangeVolume()
+    {
+        bgmVolume = bgmSlider.GetComponent<Slider>().normalizedValue;
+        seVolume = seSlider.GetComponent<Slider>().normalizedValue;
+        Debug.Log(bgmVolume);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bgm.volume = bgmVolume;
     }
 }
