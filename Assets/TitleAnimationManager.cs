d6a7346cea28cabbc3ed36adf220322aa747e6ec
kota0126 <watako0126w@gmail.com>
2020-03-17 19:17:32 +0900

    あんま変えてないけど一応プッシュ。 ProductName変えたからPlayerPrefが初期化されてるかも。

--- Assets/TitleAnimationManager.cs
+++ Assets/TitleAnimationManager.cs
@@ -10,23 +10,46 @@ public class TitleAnimationManager : MonoBehaviour
     public GameObject endGhost;
     private Sprite Move0;
     private Sprite Move1;
+    public Sprite SecretMove0;
+    public Sprite SecretMove1;
     public int dl = 50;
     public float moveSpeed = 100;
     private Vector3 startPosition;
     public bool moveVector = true;
-    public int subStartdl = 5; 
+    public bool includeSecret;
+    public int subStartdl = 5;
+    private int rndint;
     // Start is called before the first frame update
     void Start()
     {
         startPosition = movingObject.transform.position;
         Move0 = movingObject.transform.GetChild(0).GetComponent<Image>().sprite;
         Move1 = movingObject.transform.GetChild(1).GetComponent<Image>().sprite;
+        if (!includeSecret)
+        {
+            SecretMove0 = movingObject.transform.GetChild(0).GetComponent<Image>().sprite;
+            SecretMove1 = movingObject.transform.GetChild(1).GetComponent<Image>().sprite;
+        }
         MovingAnime();
     }
 
     private void SubStart()
-    {
+    {   
         movingObject.gameObject.SetActive(true);
+
+        //100分の1の確率で別スキンのキャラが出現する
+        rndint = Random.Range(0,30);
+        if (rndint == 0)
+        {
+            Move0 = SecretMove0;
+            Move1 = SecretMove1;
+        }
+        else
+        {
+            Move0 = movingObject.transform.GetChild(0).GetComponent<Image>().sprite;
+            Move1 = movingObject.transform.GetChild(1).GetComponent<Image>().sprite;
+        }
+        
         movingObject.transform.position = startPosition;
     }
 
