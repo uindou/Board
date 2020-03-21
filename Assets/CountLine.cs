74c9e43b195c02d54a6884df04c895a80a2d49c9
kota0126 <watako0126w@gmail.com>
2020-03-21 15:41:13 +0900

    全スクリプトの行数をカウントするやつ拾った。window => LineCount => Count All Script Line でコンソールに表示される。

---
+++ Assets/CountLine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+
+public class CountLine : EditorWindow
+{
+
+    [MenuItem("Window/LineCount")]
+    public static void Init()
+    {
+        CountLine window = EditorWindow.GetWindow<CountLine>("CountLine");
+        window.Show();
+        window.Focus();
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Count All Script line"))
+        {
+            DirectoryInfo dir = new DirectoryInfo(Application.dataPath);
+            FileInfo[] info = dir.GetFiles("*.cs", System.IO.SearchOption.AllDirectories);
+            if (info == null || info.Length == 0)
+            {
+                return;
+            }
+
+            int allLineCount = 0;
+            info.Where((x) => x.Name != "CountLine.cs").ToList().ForEach((x) => {
+
+                using (StreamReader sr = x.OpenText())
+                {
+                    while (sr.ReadLine() != null)
+                    {
+                        allLineCount++;
+                    }
+                }
+            });
+
+            Debug.LogFormat("Number of All Scripts line is {0}", allLineCount);
+
+        }
+    }
+}
